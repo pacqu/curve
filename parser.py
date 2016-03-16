18986556846a861197b8d276131faf9c8b79bdb3
Justin Pacquing <pacqqu@gmail.com>
2016-03-16 13:51:23 -0400

    start

--- parser.py
+++ parser.py
@@ -3,5 +3,10 @@ from matrix import *
 from draw import *
 
 def parse_file( fname, points, transform, screen, color ):
-    pass
+    fi =  open(fname, "r")
+    lines = []
+    for line in fi:
+        lines.append(line.strip('\n'))
+    return lines
 
+print parse_file("script_curves", 0, 0, 0, 0)
